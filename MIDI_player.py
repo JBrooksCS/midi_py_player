71f32d05eb405fdbc539f10570d0751f6ee405e7
JBrooksCS <jasonbrooksbio@gmail.com>
2019-07-30 20:19:36 -0500

    Messing with some alternative approaches

--- MIDI_player.py
+++ MIDI_player.py
@@ -1,70 +1,71 @@
 import mido
 from mido import MidiFile, MetaMessage
 from mido.ports import MultiPort
-
-import time
-
+#Open Input
 port = mido.open_input('nanoKEY2 0')
+#Open Output
+outport = mido.open_output('Microsoft GS Wavetable Synth 0')
+#Load MIDI Files
+SMF_1 = MidiFile("Bee_Gees_-_Stayin_Alive-Voice.mid")
+SMF_2 = MidiFile("Movie_Themes_-_Jurassic_Park.mid")
 
 
-def counter(message):
-    print('Made it to counter')
-    msg = port.poll()
-    print(msg)
-    while(msg != 'None'):
-        print('Waiting...')
-        time.sleep(1)
-        msg = port.poll()
 
-
+#Handler definition
+def play_SMF(SMF_file):
+    print('Playing MIDI File. . . ')
+    global outport
+    if(outport.closed):
+        outport = mido.open_output('Microsoft GS Wavetable Synth 0')    
+    #for msg in MidiFile("Bee_Gees_-_Stayin_Alive-Voice.mid").play():
+    for msg in SMF_file.play():
+        print(msg)
+        outport.send(msg)
+        incomingMsg = port.poll() 
+        if (incomingMsg):
+            #print(incomingMsg.type)
+            if incomingMsg.note == 70 and incomingMsg.type == 'note_off':
+                outport.reset()           
+                break
 
 #Callback definition
 def route_midi(message):
-    print('Made it to route midi')
     if message.type == 'note_on':
         if message.note == 72:
             print('Key 72')
-            
+            play_SMF(SMF_1)
         elif message.note == 71:
-            counter(message)
-            
-        
+            print('Key 71')
+            play_SMF(SMF_2)
+#Maybe put the callback here?
+def forward_while_reading(message)
 
 
-port.callback = route_midi
+#Set the ports callback function
+#port.callback = route_midi
 
 
-#SCRATCH
-
-import mido
-from mido import MidiFile, MetaMessage
-from mido.ports import MultiPort
-
-port = mido.open_input('nanoKEY2 0')
-outport = mido.open_output('Microsoft GS Wavetable Synth 0')
-
-
-############################################################
+#Twiddle your thumbs while you wait for a note
 while True:
-    for msg in port.iter_pending():
-        print(msg)
-
-    do_other_stuff()
-######################################################
-###DIS IS IT - PLAY IT TILL I QUIT
-#timeCounter = 0.0
-for msg in MidiFile("Movie_Themes_-_Jurassic_Park.mid").play():
-    outport.send(msg)
-    incomingMsg = port.poll() 
-    if (incomingMsg):
-        print(incomingMsg.type)
-        if incomingMsg.note == 70 and incomingMsg.type == 'note_off':
-            outport.reset()           
-            break
+    #print ('Twiddlin')
+    msg_seeker = port.poll()
+    if(msg_seeker):
+        route_midi(msg_seeker)
 
 
+# ##################SCRAP
+# Calling receive(), __iter__(), or iter_pending() on a port with a callback will raise an exception:
 
+# ValueError: a callback is set for this port
+# To clear the callback:
 
-        #timeCounter += msg.time
-        #print(timeCounter)
+# port.callback = None
 
+# for msg in MidiFile('Movie_Themes_-_Jurassic_Park.mid'):
+#     if(msg.is_meta):
+#         if msg.type == 'set_tempo':
+#             print(msg.tempo)
+
+#             tempo2bpm()
+
+# mid = MidiFile('Movie_Themes_-_Jurassic_Park.mid')
